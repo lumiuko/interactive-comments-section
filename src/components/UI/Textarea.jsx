6395b114b99bd851b3abf48cc73f4b87bda145a7
David <kudavidss@gmail.com>
2024-02-02 03:44:27 +0400

    placeholder color change

--- src/components/UI/Textarea.jsx
+++ src/components/UI/Textarea.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react'
 const Textarea = forwardRef((props, ref) => (
   <textarea
     {...props}
-    className="flex-1 px-6 py-3 rounded-lg border-[1px] border-x-light-gray min-h-24 text-dark-blue resize-none outline-moderate-blue"
+    className="flex-1 px-6 py-3 rounded-lg placeholder:text-grayish-blue border-[1px] border-x-light-gray min-h-24 text-dark-blue resize-none outline-moderate-blue"
     ref={ref}
   />
 ))
